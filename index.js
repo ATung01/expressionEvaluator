8110c2dd0d4726ca13258b0aa82ad368720d2c4e
github username <acc.tung@gmail.com>
2018-02-28 14:36:54 -0500

    adding and subtracting done

--- index.js
+++ index.js
@@ -2,13 +2,39 @@ $(function() {
 
   $("#expInput").submit(function( event ) {
     event.preventDefault();
-    let result = $("#expString").val()
+    let input = $("#expString").val().split(" ")
+    let result = expEval(input)
     console.log(result)
   });
 
 });
 
 
-function expEval() {
-  
+function expEval(arrOfExp) {
+
+  let numStack = []
+
+  if (arrOfExp.length === 1) {
+    return arrOfExp[0]
+  }
+
+ // it's conceptually easier for me to work on this array from the end to the beginning
+  for (let i = arrOfExp.length; i >= 0; i--) {
+    console.log(numStack)
+
+    let a = parseInt(numStack.pop())
+    let b = parseInt(numStack.pop())
+    if (arrOfExp[i] === "+") {
+      numStack.push(b + a)
+    }
+    else if (arrOfExp[i] === "-") {
+      numStack.push(a - b)
+    }
+    else {
+      numStack.push(b, a, arrOfExp[i])
+    }
+  }
+
+  console.log(numStack)
+  return numStack.pop()
 }
